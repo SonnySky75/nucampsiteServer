f69a82638f6f070b25fac2d8398235215a73bb23
SonnySky75 <85204037+SonnySky75@users.noreply.github.com>
2021-11-28 18:15:41 -0600

    created favoriteRouter.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -25,7 +26,6 @@ const connect = mongoose.connect(url, {
 connect.then(() => console.log('Connected correctly to server'), 
     err => console.log(err)
 );
-
 
 var app = express();
 
@@ -48,21 +48,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-
 app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
-
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
